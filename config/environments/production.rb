115f7c3e245a799a3c3bab4458681689ad5e9f98
Emilio Alvarez <emilioa84@yahoo.com>
2015-08-21 16:34:27 -0700

    updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
     
-config.action_mailer.default_url_options = { :host => 'pinteresting-emilioapp.herokuapp.com/' }    
+config.action_mailer.default_url_options = { :host => 'http://pinteresting-emilioapp.herokuapp.com/' }    
 end
